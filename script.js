b8b3bdc0bd57847cf13277d486d6fc7de8e0b5e5
Christopher Todd <christophertodd0509@icloud.com>
2024-09-14 15:07:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function dropToggle(id, buttonImage) {
+    let ele = document.getElementById(id)
+    let img = document.getElementById(buttonImage)
+    if (ele.style.display === "block") {
+        ele.style.display = "none";
+        img.style.transform = "rotate(90deg)";
+    } else {
+        ele.style.display = "block";
+        img.style.transform = "rotate(180deg)";
+    }
+}
